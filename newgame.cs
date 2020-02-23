027b7de80ab5bf01fc94177eb2097230ba810321
Bint.e.Zahra12 <bint.e.zahra1255@gmail.com>
2020-02-23 19:16:24 +0500

    Added new files

---
+++ newgame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newgame : MonoBehaviour
+{
+    public void TakeNewGame(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+
+
+
+    public void OptionMenu(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+
+
+    public void BackToMenu(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
---
+++ pausemenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pausemenu : MonoBehaviour
+{
+
+   
+    public static bool PauseGame = false;
+    public GameObject PauseMenuUI;
+    public static bool gamequit = false;
+    public GameObject quitgame;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pasue();
+            }
+        }
+
+       
+    }
+
+
+    public void Resume()
+    {
+
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+    }
+
+    void Pasue()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void LoadMenu(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+    public void QuitGame()
+    {
+        quitgame.SetActive(false);
+        Time.timeScale = 1f;
+        gamequit = false;
+    }
+}
